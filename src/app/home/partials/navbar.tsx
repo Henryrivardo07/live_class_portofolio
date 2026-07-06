2fac54a4369841a8fd7cf11e4a49494f670f48a8
Henry Rivardo <henrysihombing20@gmail.com>
2026-07-06 20:25:18 +0700

    feat:navbar section

---
+++ src/app/home/partials/navbar.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Navbar</h1>
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/button.tsx
@@ -0,0 +1,48 @@
+import { Button as ButtonPrimitive } from "@base-ui/react/button";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "flex justify-center items-center shrink-0 disabled:pointer-events-none disabled:opacity-50 cursor-pointer outline-none",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary-300 text-sm-medium text-neutral-25 rounded-full hover:shadow-[0_0_23px_rgba(0,147,221,0.6)]",
+        outline:
+          "border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
+      },
+      size: {
+        default: "h-11 px-14.5",
+        icon: "size-8",
+        "icon-xs":
+          "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
+        "icon-sm":
+          "size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg",
+        "icon-lg": "size-9",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+function Button({
+  className,
+  variant = "default",
+  size = "default",
+  ...props
+}: ButtonPrimitive.Props & VariantProps<typeof buttonVariants>) {
+  return (
+    <ButtonPrimitive
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Button, buttonVariants };

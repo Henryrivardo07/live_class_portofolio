641451d0447b383941cbcaf4aa1722f37f384a4b
Henry Rivardo <henrysihombing20@gmail.com>
2026-07-06 21:51:27 +0700

    feat: mobile navigation

--- src/app/home/partials/navbar.tsx
+++ src/app/home/partials/navbar.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 import { navigationData } from "@/constant/navigation-data";
+import { Menu } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -34,6 +41,37 @@ const Navbar = () => {
         <Button asChild className="hidden lg:flex">
           <Link href="#contact">Get Started</Link>
         </Button>
+
+        <Sheet>
+          <SheetTrigger>
+            <Menu
+              className="cursor-pointer lg:hidden"
+              aria-label="Open main navigation"
+            />
+          </SheetTrigger>
+          <SheetContent>
+            <nav className="mt-16" aria-label="Mobile Navigation">
+              <ul className="flex flex-col gap-4">
+                {navigationData.map((data) => (
+                  <li key={data.label}>
+                    <Link
+                      className="hover:text-primary-200 p-4"
+                      href={data.href}
+                    >
+                      {data.label}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+
+            <Button asChild className="mt-3 w-full">
+              <SheetClose asChild>
+                <Link href="#contact">Get Started</Link>
+              </SheetClose>
+            </Button>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
